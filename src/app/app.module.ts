c8f1b857a3eba0cef943707a22c1b590de0bed93
Rolando Fender <92167269+Fender1992@users.noreply.github.com>
2024-02-23 13:25:43 -0600

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { Routes } from '@angular/router';
+import { ProfileComponent } from './profile-component/profile-component.component';
 
+const appRoutes: Routes = [{ path: 'profiles', component: ProfileComponent }];
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, ProfileComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/profile-component/profile-component.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-component',
+  templateUrl: './profile-component.component.html',
+  styleUrl: './profile-component.component.css',
+})
+export class ProfileComponent {}
